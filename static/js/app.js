297d58d4bc9b2d70756cdaa4236dddae43253853
Vero Castillo <77795761+verocastillo@users.noreply.github.com>
2021-07-02 13:22:10 -0500

    Didn't work. Trying new code.

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,15 @@
-// Import belly button dataset
-d3.json("data.json").then((bbData) => {
-
-    // Function to fill dropdown menu with options
-    function dropdownOp() {
-        var subnames = bbData.names;
+// Function to fill dropdown menu with options
+function init() {
+    d3.json("data/samples.json").then((data) => {
+        var subnames = data.names;
         var dropsel = d3.select("#selDataset")
         subnames.forEach((name => {
             var option = dropsel.append("option");
             option.text(name);
+            console.log(option);
         }));
-    }
+    })
+}
 
-});
+// Run initialization
+//init()
