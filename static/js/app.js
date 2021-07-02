db4711d33f5c4d060d184e240c0ec0a557edbaab
Vero Castillo <77795761+verocastillo@users.noreply.github.com>
2021-07-02 12:59:33 -0500

    Dropdown menu done.

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,14 @@
 // Import belly button dataset
-var bbdata = data;
+d3.json("data.json").then((bbData) => {
 
+    // Function to fill dropdown menu with options
+    function dropdownOp() {
+        var subnames = bbData.names;
+        var dropsel = d3.select("#selDataset")
+        subnames.forEach((name => {
+            var option = dropsel.append("option");
+            option.text(name);
+        }));
+    }
+
+});
