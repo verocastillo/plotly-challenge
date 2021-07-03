14d7538c44e40298d6221f0da306efa567774453
Vero Castillo <77795761+verocastillo@users.noreply.github.com>
2021-07-03 13:01:58 -0500

    Added more detail to comments. Started demographic box.

--- static/js/app.js
+++ static/js/app.js
@@ -1,22 +1,29 @@
 // Function to fill dropdown menu with options
 function init() {
+    // Select the data from samples.json and get id names
     d3.json("data/samples.json").then((data) => {
         var subnames = data.names;
+        // Append each name to the dropdown options
         var dropsel = d3.select("#selDataset")
         subnames.forEach((name => {
             var option = dropsel.append("option");
             option.text(name);
-            console.log(option);
         }));
     })
 }
 
 // Event handlers: change page on trigger
-var button = d3.select("#selDataset");
-button.on('change', runEnter);
+    // Select dropdown as button trigger
+    var button = d3.select("#selDataset");
+    // Configure so function runs when value is changed
+    button.on('change', runEnter);
 
 // Function to obtain graphs and information
-
+function runEnter() {
+    // Reads the ID value from the dropdown
+    var selectid = parseInt(button.property("value"));
+    //
+}
 
 // Run initialization
 init()
